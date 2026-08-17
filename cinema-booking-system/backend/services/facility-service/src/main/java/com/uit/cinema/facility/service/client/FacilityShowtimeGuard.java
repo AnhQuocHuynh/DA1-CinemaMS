291ed33444e7c4be34b7061b0ac2e20dd9b004a5
ViciousEve <146147992+ViciousEve@users.noreply.github.com>
2026-08-17 21:05:25 +0700

    remove facility-service spring boot impl

--- cinema-booking-system/backend/services/facility-service/src/main/java/com/uit/cinema/facility/service/client/FacilityShowtimeGuard.java
+++
@@ -1,8 +0,0 @@
-package com.uit.cinema.facility.service.client;
-
-import java.util.List;
-
-public interface FacilityShowtimeGuard {
-    boolean hasFutureShowtimesForRoom(Long roomId);
-    boolean hasFutureShowtimesForRooms(List<Long> roomIds);
-}
